2c613a04ea8029c67a0ef6ab6bb78d3d90beb890
Jared Selasie Gallant <jaredselasiegallant-sudo@users.noreply.github.com>
2026-07-30 20:41:14 +0100

    Fix CI: add asset dirs with .gitkeep, set explicit Android SDK versions (compileSdk 36, minSdk 21)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.pocketledger.pocket_ledger"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,12 +15,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.pocketledger.pocket_ledger"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
