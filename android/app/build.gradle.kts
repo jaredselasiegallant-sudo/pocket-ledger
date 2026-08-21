b885f2287eb99d143afe15363b2480e0c6282878
MR. SELASIE <mr.selasie@users.noreply.github.com>
2026-08-21 10:34:48 +0100

    fix: add asset directories and bump Android compileSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.pocketledger.pocket_ledger"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
